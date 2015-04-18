2a85e4f7b80184f4834dca844b8314970b8e0332
Christian Haug <chris-haug@gmx.de>
2015-04-17 22:18:15 -0400

    First version of webscraping app with node.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var url = 'https://news.ycombinator.com/';
+
+request(url, function(error, response, body){
+
+	var $ = cheerio.load(body);
+	var rows = $('table table tr');
+
+	console.log(rows.text());
+});
---
+++ app2.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var url = 'https://news.ycombinator.com/';
+
+request(url, function(error, response, body){
+
+	var $ = cheerio.load(body);
+
+	var titleContainers = $('td.title').has('.sitebit.comhead');
+
+	var data = [];
+
+	// equivalent to a for loop
+  	titleContainers.each(function(index, titleContainer) {
+    var titleLink = $(titleContainer).find('a');
+
+    var comments = $(titleContainer).parent().next().find('.subtext');
+
+    data[index] = {
+      title: titleLink.text(),
+      link: titleLink.attr('href'),
+      source: $(titleContainer).find('.sitebit.comhead').text(),
+      score: comments.find('.score').text(),
+      submitter: $(comments.find('a').get(0)).text(),
+      numberComments: $(comments.find('a').get(1)).text(),
+	 	};
+	 });
+
+
+
+	console.log(data);
+});
